67e06a10dd6ce0da813957e161912b23d472fe2c
Taras Polovyi <taras.polovyi@ua.ibm.com>
2018-06-18 13:34:15 +0300

    Just redux features

--- src/Components/PostForm.js
+++ src/Components/PostForm.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'; 
+import {connect} from 'react-redux';
+import {createPost} from '../actions/postActions';
 
 class PostForm extends Component {
     constructor(props){
@@ -21,15 +24,9 @@ onSubmit(e){
        title: this.state.title,
        body:this.state.body
    }
-   fetch('https://jsonplaceholder.typicode.com/posts',{
-       method:'POST',
-       headers:{
-           'content-type':'application/json'
-       },
-       body:JSON.stringify(post)
-   })
-   .then(res=>res.json())
-   .then(data=>console.log(data));
+
+ this.props.createPost(post);
+
 }
 
     render(){
@@ -55,5 +52,9 @@ onSubmit(e){
     }
 }
 
+PostForm.propTypes = {
+    createPost:PropTypes.func.isRequired
+};
 
-export default PostForm;
+
+export default connect(null,{createPost})(PostForm);
